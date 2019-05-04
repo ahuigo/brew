f21adc70a6eefe70ed59b1b1cebcbdbc77294b30
ahuigo <a132811@gmail.com>
2019-05-04 10:32:54 +0800

    Create v2ray-core.rb

---
+++ v2ray-core.rb
@@ -0,0 +1,61 @@
+class V2rayCore < Formula
+  desc "A platform for building proxies to bypass network restrictions."
+  homepage "https://www.v2ray.com/"
+  url "https://github.com/v2ray/v2ray-core/releases/download/v4.18.0/v2ray-macos.zip"
+  version "4.18.0"
+  sha256 "00575a262e833e29222882ec9d75c82d42bb324a6e47e7815d7e0142c3675a8b"
+
+  # depends_on "cmake" => :build
+
+  def install
+
+    bin.install "v2ray"
+    bin.install "v2ctl"
+    bin.install "geoip.dat"
+    bin.install "geosite.dat"
+
+    (etc/"v2ray").mkpath
+    etc.install "config.json" => "v2ray/config.json"
+
+  end
+
+  plist_options :manual => "v2ray -config=#{HOMEBREW_PREFIX}/etc/v2ray/config.json"
+
+  def plist; <<~EOS
+  <?xml version="1.0" encoding="UTF-8"?>
+  <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+  <plist version="1.0">
+    <dict>
+      <key>KeepAlive</key>
+      <true/>
+      <key>RunAtLoad</key>
+      <true/>
+      <key>Label</key>
+      <string>#{plist_name}</string>
+      <key>ProgramArguments</key>
+      <array>
+        <string>#{bin}/v2ray</string>
+        <string>-config</string>
+        <string>#{etc}/v2ray/config.json</string>
+      </array>
+    </dict>
+  </plist>
+  EOS
+  end
+
+
+
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test v2ray-core`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "#{bin}/v2ray", "-version"
+  end
+end
